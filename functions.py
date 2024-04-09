9916742cdba89d06f48cf6e6dbaeff8dede1354b
AsgharGulkala <155817077+AsgharGulkala@users.noreply.github.com>
2024-04-10 02:38:00 +0500

    Day 11 to Onward
    Day 11 to Onward

---
+++ functions.py
@@ -0,0 +1,37 @@
+# defining a function
+
+# 1
+# def print_asghar_tech ():
+#     print("first time lerning programming")
+#     print("first time lerning programming")
+#     print("first time lerning programming")
+    
+# print_asghar_tech()
+
+# def print_asghar_tech ():
+#     text = "first time i am lerning programming"
+#     print (text)
+#     print (text)
+#     print (text)
+# print_asghar_tech ()
+
+
+
+
+# def school_calculator(age): 
+#     if age==5:
+#         print ("Congrats! Muslin can join the school")
+#     elif age>5:
+#         print ("Muslim should go to higher school")
+#     else:
+#         print ("Muslims is a baby and not allowed in this School")
+
+# school_calculator (5)
+
+# defining a future function
+def future_age (age):
+    new_age=age+20
+    return new_age
+    print (new_age)
+future_predicted_age=future_age(3)
+print(future_predicted_age)
